74bcdfafa7d07537d188a225cbd059a329921a18
unknown <quentin.peris@icloud.com>
2024-08-22 09:43:42 +0200

    Configuration bundles et flags

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   configureWebpack: {
@@ -10,15 +11,18 @@ module.exports = {
         '@views': path.resolve(__dirname, 'src/views'), 
         '@stores': path.resolve(__dirname, 'src/stores')
       }
-    }
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        '__VUE_PROD_HYDRATION_MISMATCH_DETAILS__': JSON.stringify(false)
+      })
+    ]
   },
-
 
   devServer: {
     port: 8080, 
     open: true, 
     proxy: {
-   
       '/api': {
         target: 'http://localhost:3000', 
         changeOrigin: true,
@@ -27,11 +31,9 @@ module.exports = {
     }
   },
 
-
   publicPath: process.env.NODE_ENV === 'production' ? '/your-app/' : '/',
   outputDir: 'dist',
   assetsDir: 'assets',
-
 
   lintOnSave: false, 
   productionSourceMap: false 
